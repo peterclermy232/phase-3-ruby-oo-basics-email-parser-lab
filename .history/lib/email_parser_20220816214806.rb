52829c760061abeee32524f2af2f9d49e2f52dd7
peterclermy <peterclermy232@gmail.com>
2022-08-16 21:59:02 +0300

    test done

--- .history/lib/email_parser_20220816214806.rb
+++ .history/lib/email_parser_20220816214806.rb
@@ -3,13 +3,4 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
-class EmailAddressParser
-    attr_reader :emails
-    def initialize(emails)
-        @emails = emails
-    end
-
-    def parse
-        emails.split(/, | /).uniq 
-    end
-end
+email_addresses 
